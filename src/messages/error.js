694dbb65f9ab56b93ea55aa19e34bd7c421963c5
Duda Furtado <furtadoduda@hotmail.com>
2022-04-30 02:52:22 -0300

    Some messages added to return in json

--- src/messages/error.js
+++ src/messages/error.js
@@ -1,5 +1,8 @@
 const message = {
-
+    nameDontExists: 'O nome não foi informado',
+    emailDontExists: 'É preciso informar um email',
+    passwordDontExists: 'A senha é essencial para segurança da sua conta',
+    cepDontExists: 'O cep é um campo obrigatório',
 }
 
 module.exports = message
--- src/messages/success.js
+++ src/messages/success.js
@@ -1,5 +1,7 @@
 const message = {
-
+    userCreated: 'O cadastro foi concluído',
+    userIn: 'Login confirmado',
+    userUpdated: 'Usuário atualizado com sucesso',
 }
 
 module.exports = message
