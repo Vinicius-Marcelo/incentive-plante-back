5d190018e193472962ea2a03fa5b06414442b574
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2022-05-01 19:25:12 -0300

    Cors working and database right

--- src/database/query.sql
+++ src/database/query.sql
@@ -1,15 +1,17 @@
-create database incentive_plante;
+create database incentiveeplante;
 
 create table users (
     id serial primary key,
     name text not null,
     email text not null,
-    password text not null,
-    cep text not null
+    idade integer not null,
+    cep text not null,
+    password text not null
 );
 
 create table trees (
     id serial primary key,
+    usuario_id integer references users(id),
     name text not null,
     region text not null,
     biome text not null
